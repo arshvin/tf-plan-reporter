009596e719382ad3b1e0ef87a196dc72dc0802b9
Ivan Poliakov <arschvin@gmail.com>
2024-01-24 21:25:54 +0100

    Added couple of words to config file explanation

--- internal/config/example.go
+++ internal/config/example.go
@@ -1,16 +1,16 @@
 package config
 
 var exampleConfig = `#tf-plan-reporter tool example config file
-terraform_binary_file: /usr/bin/terraform     # Absolute or relative path of terraform command
-terraform_plan_file_basename: plan.bin        # Base name of terraform binary file for further search
-terraform_plan_search_folder: .               # Common parent folder from which to start search of generated plan files
+terraform_binary_file: /usr/bin/terraform     # Absolute or relative path of terraform command. MANDATORY parameter
+terraform_plan_file_basename: plan.bin        # Base name of terraform binary file for further search. MANDATORY parameter
+terraform_plan_search_folder: .               # Common parent folder from which to start search of generated plan files. MANDATORY parameter
 
 # List of cloud resources which should be kept. Must be either "all" and then the "allowed_removals" section of this file
-# is going to payed attention. Or it should contain particular type of resources which should be kept from accidental removals
+# is going to payed attention. Or it should contain particular type of resources which should be kept from accidental removals. OPTIONAL parameter
 critical_resources:
   - all
 
-allowed_removals:                             # Makes sense only if "all" item is specified in "critical_resources" section
+allowed_removals:                             # Makes sense only if "all" item is specified in "critical_resources" section. OPTIONAL parameter
   - null_resource
   - azurerm_role_assignment
   - azurerm_monitor_diagnostic_setting
